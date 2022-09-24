fcca94d742d4e0a78dbce6b737e86605d549d188
EC2 Default User <ec2-user@ip-172-31-27-64.us-west-2.compute.internal>
2022-09-24 09:31:17 +0000

    課題提出修正

--- main.js
+++ main.js
@@ -1,129 +1,68 @@
-/*global $*/ 
-$(document).ready(function(){
-	// 変数定義
-	let num_1 ;
-	let num_2;
-	let math;
-	let minus;
-	let formula_count;
-	
-	num_1 = null;
-	num_2 = null;
-	math = null;
-	minus = false;
-	formula_count = 0;
-	
-	// 最初の数値と演算子入力チェック
-	$(".cal-click").click(function(){
-		// 演算子入力チェック
-		if (num_1 == null && math == null) {
-			// 前半（+, * , /）
-			if ($(this).val() == "+" || $(this).val() == "*" || $(this).val() == "/") {
-				math = null;
-				num_1 = null;
-				num_2 = null;
-				minus = false;
-				formula_count = 0;
-				return;
-			} else {
-				// 後半（-）
-				if ($(this).val() == "-") {
-					minus = true;
-					num_1 = "-";
-				}
-			}
-		}
-		// 数値入力チェック
-		if ($(this).attr("data-role") != "cal-formula") {
-			if (minus) {
-				num_1 = num_1 + $(this).val();
-				$("#display").val($("#display").val() + $(this).val());
-				formula_count = 0;
-				minus = false;
-				return;
-			}
-			
-			if (num_1 == null && math == null) {
-				$("#display").val('');
-				$("#display").val($("#display").val() + $(this).val());
-				formula_count = 0;
-				num_1 = $(this).val();
-			} else if (num_1 != null && math == null) {
-				$("#display").val($("#display").val() + $(this).val());
-				formula_count = 0;
-				num_1 = num_1 + $(this).val();
-			} else if (num_2 == null && math != null) {
-				$("#display").val($("#display").val() + $(this).val());
-				formula_count = 0;				
-				num_2 = $(this).val();
-			} else if (num_2 != null && math != null) {
-				$("#display").val($("#display").val() + $(this).val());
-				formula_count = 0;
-				num_2 = num_2 + $(this).val();
-			} else {
-				return;
-			}
-		} else {
-			formula_count ++;
-			if (formula_count == 2) {
-				formula_count == 1;
-			} else if (minus) {
-				$("#display").val('');
-				$("#display").val($("#display").val() + $(this).val());
-			} else if (formula_count == 1 && num_1 != null && num_2 == null) {
-				math = $(this).val();
-				$("#display").val($("#display").val() + $(this).val());
-			} else if (formula_count == 1 && num_1 != null && num_2 != null) {
-				calculation();
-				num_2 = null;
-				math = $(this).val();
-				$("#display").val($("#display").val() + $(this).val());
-			} else {
-				return;
-			}
-		}
-	});
-	
-	// 計算関数
-	function calculation() {
-	  switch (math) {
-			case "+":
-				num_1 = Number(num_1) + Number(num_2);
-				break;
-				case "-":
-					num_1 = Number(num_1) - Number(num_2);
-					break;
-				case "*":
-					num_1 = Number(num_1) * Number(num_2);
-					break;
-				case "/":
-					num_1 = Number(num_1) / Number(num_2);
-					break;				
-				default:
-					break;
-	  }
-	}
-	
-	// クリアキー
-	$(".clear-btn").click(function(){
-		$("#display").val('');
-		math = null;
-		num_1 = null;
-		num_2 = null;
-		minus = false;
-		formula_count = 0;
-	});
-	
-	// イコールキー
-	$(".cal-btn").click(function(){
-		calculation();
-		$("#display").val('');
-		$("#display").val(num_1);
-		math = null;
-		num_1 = null;
-		num_2 = null;
-		minus = false;
-		formula_count = 0;
-	});	
-});
+var dsp = "0";  //表示部
+var opr1;       //数字
+var operator;   //演算子
+var wasnum = false;  //最後押下キー
 
+
+//表示部処理
+function display (numkey) {
+	wasnum = numkey;
+	document.getElementById("disp").innerHTML = dsp;
+}
+
+//数字処理
+function nk(k) {
+  var ch = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."] [k];
+
+  if (! wasnum) {
+    if ( k > 9) dsp = "0.";
+    else dsp = ch;
+  } else {
+    if (k > 9) {
+      if ( dsp.indexOf(".") < 0) dsp += ".";
+    } else {
+      if (dsp === "0") {
+        dsp = ch;
+      } else {
+        dsp += ch;
+      }
+    }
+  }
+  display(true);
+}
+
+//演算キー処理
+function op(k) {
+  if (operator !== undefined &&
+  operator != 9 &&
+  (wasnum || k == 9)) calculate();
+  operator = k;
+  opr1 = parseFloat(dsp);
+  display(false);
+}
+
+//計算
+function calculate() {
+  switch(operator) {
+    case 1:
+    	opr1 += parseFloat(dsp);
+    	break;
+    case 2:
+    	opr1 -= parseFloat(dsp);
+    	break;
+    case 3:
+    	opr1 *= parseFloat(dsp);
+    	break;
+    case 4:
+    	opr1 /= parseFloat(dsp);
+    	break;
+  }
+  dsp = String(opr1);
+}
+
+//クリアキー
+function ac() {
+  dsp = "0";
+  opr1 = operator = undefined;
+  display(false);
+}
